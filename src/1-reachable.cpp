f6244841795c142845775b22334edaafbc1f6973
Brian Chipman <namedtuple@gmail.com>
2015-04-20 13:36:25 -0700

    wrote 'read_graph' function.

--- src/1-reachable.cpp
+++ src/1-reachable.cpp
@@ -9,17 +9,26 @@
 #include "array_set.hpp"
 #include "array_map.hpp"
 
-
-typedef ics::ArraySet<std::string> NodeSet;
-typedef ics::pair<std::string, NodeSet> GraphEntry;
-typedef ics::ArrayPriorityQueue<GraphEntry> GraphPQ;
-typedef ics::ArrayMap<std::string, NodeSet> Graph;
+typedef ics::ArraySet           <std::string>               NodeSet;
+typedef ics::pair               <std::string, NodeSet>      GraphEntry;
+typedef ics::ArrayPriorityQueue <GraphEntry>                GraphPQ;
+typedef ics::ArrayMap           <std::string, NodeSet>      Graph;
 
 //Read an open file of edges (node names separated by semicolons, with an
 //  edge going from the first node name to the second node name) and return a
 //  Graph (Map) of each node name associated with the Set of all node names to
 //  which there is an edge from the key node name.
 Graph read_graph (std::ifstream& file) {
+
+    Graph graph;
+    std::string line;
+
+    while (getline(file, line)) {
+        std::vector<std::string> nodes = ics::split(line, ";");
+        graph[nodes.front()].insert(nodes.back());
+    }
+    file.close();
+    return graph;
 }
 
 //Print a label and all the entries in the Graph, in alphabetical order.
@@ -42,6 +51,11 @@ NodeSet reachable (const Graph& graph, std::string start) {
 //  reachable from it using the edges in the Graph.
 int main () {
     try {
+        std::ifstream graph_data_file;
+        ics::safe_open(graph_data_file, "Enter file name", "graph1.txt");
+        read_graph(graph_data_file);
+
+
     }
     catch (ics::IcsError& e) {
         std::cout << e.what() << std::endl;
