b60417bd1ea94b1866c6aaac8f91530a45a288cc
Brian Chipman <namedtuple@gmail.com>
2015-04-21 13:35:36 -0700

    Formatting

--- src/4-ndfa.cpp
+++ src/4-ndfa.cpp
@@ -9,67 +9,66 @@
 #include "array_set.hpp"
 #include "array_map.hpp"
 
-
-typedef ics::ArraySet<std::string>                States;
-typedef ics::ArrayQueue<std::string>              InputsQueue;
-typedef ics::ArrayMap<std::string,States>         InputStatesMap;
-
-typedef ics::ArrayMap<std::string,InputStatesMap> NDFA;
-typedef ics::pair<std::string,InputStatesMap>     NDFAEntry;
-typedef ics::ArrayPriorityQueue<NDFAEntry>        NDFAPQ;
-
-typedef ics::pair<std::string,States>             Transitions;
-typedef ics::ArrayQueue<Transitions>              TransitionsQueue;
+typedef ics::ArraySet           <std::string>                   States;
+typedef ics::ArrayQueue         <std::string>                   InputsQueue;
+typedef ics::ArrayMap           <std::string, States>           InputStatesMap;
+typedef ics::ArrayMap           <std::string, InputStatesMap>   NDFA;
+typedef ics::pair               <std::string, InputStatesMap>   NDFAEntry;
+typedef ics::ArrayPriorityQueue <NDFAEntry>                     NDFAPQ;
+typedef ics::pair               <std::string, States>           Transitions;
+typedef ics::ArrayQueue         <Transitions>                   TransitionsQueue;
 
 
-//Read an open file describing the non-deterministic finite automaton (each
-//  line starts with a state name followed by pairs of transitions from that
-//  state: (input followed by a new state) all separated by semicolons), and
-//  return a Map whose keys are states and whose associated values are another
-//  Map with each input in that state (keys) and the resulting set of states it
-//  can lead to.
-const NDFA read_ndfa(std::ifstream &file) {
+const NDFA read_ndfa (std::ifstream& file) {
+    //Read an open file describing the non-deterministic finite automaton (each
+    //  line starts with a state name followed by pairs of transitions from that
+    //  state: (input followed by a new state) all separated by semicolons), and
+    //  return a Map whose keys are states and whose associated values are another
+    //  Map with each input in that state (keys) and the resulting set of states it
+    //  can lead to.
 }
 
-
-//Print a label and all the entries in the finite automaton Map, in
-//  alphabetical order of the states: each line has a state, the text
-//  "transition:" and the Map of its transitions.
-void print_ndfa(const NDFA& ndfa) {
+void print_ndfa (const NDFA& ndfa) {
+    //Print a label and all the entries in the finite automaton Map, in
+    //  alphabetical order of the states: each line has a state, the text
+    //  "transition:" and the Map of its transitions.
 }
 
-
-//Return a queue of the calculated transition pairs, based on the non-deterministic
-//  finite automaton, initial state, and queue of inputs; each pair in the returned
-//  queue is of the form: input, set of new states.
-//The first pair contains "" as the input and the initial state.
-//If any input i is illegal (does not lead to any state in the non-deterministic finite
-//  automaton, ignore it).
-TransitionsQueue process(const NDFA& ndfa, std::string state, const InputsQueue& inputs) {
+TransitionsQueue process (const NDFA& ndfa, std::string state, const InputsQueue& inputs) {
+    //Return a queue of the calculated transition pairs, based on the non-deterministic
+    //  finite automaton, initial state, and queue of inputs; each pair in the returned
+    //  queue is of the form: input, set of new states.
+    //The first pair contains "" as the input and the initial state.
+    //If any input i is illegal (does not lead to any state in the non-deterministic finite
+    //  automaton, ignore it).
 }
 
-
-//Print a TransitionsQueue (the result of calling process) in a nice form.
-//Print the Start state on the first line; then print each input and the
-//  resulting new states indented on subsequent lines; on the last line, print
-//  the Stop state.
-void interpret(TransitionsQueue& tq) {  //or TransitionsQueue or TransitionsQueue&&
+void interpret (TransitionsQueue& tq) {  //or TransitionsQueue or TransitionsQueue&&
+    //Print a TransitionsQueue (the result of calling process) in a nice form.
+    //Print the Start state on the first line; then print each input and the
+    //  resulting new states indented on subsequent lines; on the last line, print
+    //  the Stop state.
 }
 
+int main () {
+    //Prompt the user for a file, create a finite automaton Map, and print it.
+    //Prompt the user for a file containing any number of simulation descriptions
+    //  for the finite automaton to process, one description per line; each
+    //  description contains a start state followed by inputs, all separated by
+    //  semicolons.
+    //Repeatedly read a description, print that description, put each input in a
+    //  Queue, process the Queue and print the results in a nice form.
+
+    try {
 
 
-//Prompt the user for a file, create a finite automaton Map, and print it.
-//Prompt the user for a file containing any number of simulation descriptions
-//  for the finite automaton to process, one description per line; each
-//  description contains a start state followed by inputs, all separated by
-//  semicolons.
-//Repeatedly read a description, print that description, put each input in a
-//  Queue, process the Queue and print the results in a nice form.
-int main() {
-  try {
-  } catch (ics::IcsError& e) {
-    std::cout << e.what() << std::endl;
-  }
 
-  return 0;
+
+    }
+
+    catch (ics::IcsError& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
 }
