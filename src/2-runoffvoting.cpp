3bdfd8cbb34d7463b14f87709e76d1c818253d04
Brian Chipman <namedtuple@gmail.com>
2015-04-20 22:07:04 -0700

    Autoformat.

--- src/2-runoffvoting.cpp
+++ src/2-runoffvoting.cpp
@@ -11,41 +11,38 @@
 #include "array_map.hpp"
 
 
-typedef ics::ArrayQueue<std::string>              CandidateQueue;
-typedef ics::ArraySet<std::string>                CandidateSet;
-typedef ics::ArrayMap<std::string,int>            CandidateTally;
+typedef ics::ArrayQueue<std::string> CandidateQueue;
+typedef ics::ArraySet<std::string> CandidateSet;
+typedef ics::ArrayMap<std::string, int> CandidateTally;
 
-typedef ics::ArrayMap<std::string,CandidateQueue> Preferences;
-typedef ics::pair<std::string,CandidateQueue>     PreferencesEntry;
+typedef ics::ArrayMap<std::string, CandidateQueue> Preferences;
+typedef ics::pair<std::string, CandidateQueue> PreferencesEntry;
 
-typedef ics::pair<std::string,int>                TallyEntry;
-typedef ics::ArrayPriorityQueue<TallyEntry>       TallyEntryPQ;
-
+typedef ics::pair<std::string, int> TallyEntry;
+typedef ics::ArrayPriorityQueue<TallyEntry> TallyEntryPQ;
 
 //Read an open file stating voter preferences (each line is (a) a voter
 //  followed by (b) all the candidates, in preference order (from most to
 //  least preferred), separated by a semicolons), and return a Map of the
 //  preferences as a Map whose keys are voter names and whose values are
 //  a queue of candidate preferences.
-Preferences read_voter_preferences(std::ifstream &file) {
+Preferences read_voter_preferences (std::ifstream& file) {
 }
-
 
 //Print a label and all the entries in the preferences Map, in alphabetical
 //  order according to the voter.
 //Use a "->" to separate the voter name from the Queue of candidate names.
-void print_voter_preferences(const Preferences& preferences) {
+void print_voter_preferences (const Preferences& preferences) {
 }
-
 
 //Print the message followed by all the entries in the CandidateTally, in
 //   the order specified by *has_higher_priority: i is printed before j, if
 //   has_higher_priority(i,j) returns true.
 //Use a "->" to separate the candidat name from the number of votes they
 //  received.
-void print_tally(std::string message, const CandidateTally& tally, bool (*has_higher_priority)(const TallyEntry& i,const TallyEntry& j)) {
+void print_tally (std::string message, const CandidateTally& tally,
+                  bool (* has_higher_priority) (const TallyEntry& i, const TallyEntry& j)) {
 }
-
 
 //Return the CandidateTally: a Map of candidates (as keys) and the number of
 //  votes they received, based on the unchanging Preferences (read from the
@@ -53,17 +50,15 @@ void print_tally(std::string message, const CandidateTally& tally, bool (*has_hi
 //Every possible candidate should appear as a key in the resulting tally.
 //Each voter should tally one vote: for their highest-ranked candidate who is
 //  still in the the election.
-CandidateTally evaluate_ballot(const Preferences& preferences, const CandidateSet& candidates) {
+CandidateTally evaluate_ballot (const Preferences& preferences, const CandidateSet& candidates) {
 }
-
 
 //Return the Set of candidates who are still in the election, based on the
 //  tally of votes: compute the minimum number of votes and return a Set of
 //  all candidates receiving more than that minimum; if all candidates
 //  receive the same number of votes, the empty Set is returned.
-CandidateSet remaining_candidates(const CandidateTally& tally) {
+CandidateSet remaining_candidates (const CandidateTally& tally) {
 }
-
 
 //Prompt the user for a file, create a voter preference Map, and print it.
 //Determine the Set of all the candidates in the election, from this Map.
@@ -76,10 +71,11 @@ CandidateSet remaining_candidates(const CandidateTally& tally) {
 //  continue this process until there are less than 2 candidates.
 //Print the final result: there may 1 candidate left, the winner, or 0, no
 //  winner.
-int main() {
-  try {
-  } catch (ics::IcsError& e) {
-    std::cout << e.what() << std::endl;
-  }
-  return 0;
+int main () {
+    try {
+    }
+    catch (ics::IcsError& e) {
+        std::cout << e.what() << std::endl;
+    }
+    return 0;
 }
